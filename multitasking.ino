8e07df5285763831016a8e19b060a082fff2ea31
ManuReval <80339356+ManuReval@users.noreply.github.com>
2021-03-20 15:23:22 +0530

    Update multitasking.ino

--- multitasking.ino
+++ multitasking.ino
@@ -4,7 +4,7 @@ unsigned long current_val_ms;
 unsigned long task20_ms;
 unsigned long task500_ms;
 
-#define TASK_20MS_ACTIVE 20
+#define TASK_20MS_ACTIVE 100
 #define TASK_500MS_ACTIVE 500
 
 int switch_state=0;
@@ -26,13 +26,8 @@ if( (current_val_ms - task20_ms) >= TASK_20MS_ACTIVE )
   task20_ms = current_val_ms;
   if(digitalRead(7) == LOW)
   {
-    switch_state = 1 ;
+    switch_state++;
   }
-
-    else
-    {
-    switch_state = 0;
-    }
 }
 
 //500ms task
@@ -45,6 +40,7 @@ if( (current_val_ms - task500_ms) >= TASK_500MS_ACTIVE )
   {
     led_state = led_state^1;
     digitalWrite(10,led_state);
+    switch_state--;
   }
 
   else
